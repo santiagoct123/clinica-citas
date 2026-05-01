82062b61aeb39125dd47fa1489c0ecb826413dad
sct <scastanedat1@academia.usbbog.edu.co>
2026-05-01 14:16:10 -0500

    fix create_all render

--- app.py
+++ app.py
@@ -221,9 +221,9 @@ def perfil():
 
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)

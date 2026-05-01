2841ca16dfa2ae386fe39b31d2bc8bea79133cb7
sct <scastanedat1@academia.usbbog.edu.co>
2026-05-01 13:27:49 -0500

    Segundo Commit: servidor de todo supongoo

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 bcrypt = Bcrypt(app)

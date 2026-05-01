066fbd8a6c2c04ab78283c29ba01bfbe69d46f39
sct <scastanedat1@academia.usbbog.edu.co>
2026-05-01 13:56:15 -0500

    fix database_url none

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ app.config['SECRET_KEY'] = 'secretkey123'
 
 database_url = os.environ.get("DATABASE_URL")
 
-if database_url.startswith("postgres://"):
+if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace(
         "postgres://",
         "postgresql://",
         1
     )
+
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
